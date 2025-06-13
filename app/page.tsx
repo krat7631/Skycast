3d07aa7a43d17991cbbd5e1637217bf0821c0067
devansh7603 <devanshnayak76@gmail.com>
2025-06-13 15:55:36 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import WeatherForm from "@/components/WeatherForm";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-4">
+      <WeatherForm />
+    </main>
+  );
+}
